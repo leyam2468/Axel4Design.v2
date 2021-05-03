2d306ac6755d24c9e32a23a977227a3e3ad36eab
leyam2468 <leyamm3@gmail.com>
2021-05-03 11:35:35 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	purge: [
+		'./index.html',
+		'./**/*.html',
+		'./pages/**/*.html',
+		'./**/*.js',
+		'./**/*.{vue,js,ts,jsx,tsx}',
+	],
 	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
