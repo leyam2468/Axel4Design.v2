0a09e562fd548151fe2c7e53fd0f6ec33c3ae965
leyam2468 <leyamm3@gmail.com>
2021-05-04 09:30:13 +0200

    add darkmood in local storge

--- js/main.js
+++ js/main.js
@@ -45,11 +45,29 @@ helper.addEventListener('click', () => {
 	helperSpans[1].classList.toggle('-translate-y-28');
 	helperSpans[2].classList.toggle('-translate-y-42');
 });
+
 // DarkMode
+if (
+	localStorage.theme === 'dark' ||
+	(!'theme' in localStorage &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+	document.querySelector('html').classList.add('dark');
+} else if (localStorage.theme === 'dark') {
+	document.querySelector('html').classList.add('dark');
+}
 darkmode.addEventListener('click', () => {
-	document.querySelector('html').classList.toggle('dark');
-	darkmode.classList.toggle('bg-red-800');
+	let htmlClasses = document.querySelector('html').classList;
+	if (localStorage.theme == 'dark') {
+		htmlClasses.remove('dark');
+		localStorage.removeItem('theme');
+	} else {
+		htmlClasses.add('dark');
+		localStorage.theme = 'dark';
+		darkmode.classList.toggle('bg-red-800');
+	}
 });
+
 // ScrolToTOp
 ScrolToTop.addEventListener('click', () => {
 	var scrollToTop = window.setInterval(function () {
