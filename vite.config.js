9ffd0bbf24d32526e006c9c1b51b978eba2713ec
leyam2468 <leyamm3@gmail.com>
2021-05-07 19:37:55 +0200

    add clock and calc

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path');
+
+module.exports = {
+	build: {
+		cssCodeSplit: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, '/index.html'),
+				clock: resolve(__dirname, '/pages/clock/index.html'),
+				calculater: resolve(__dirname, '/pages/calculater/index.html'),
+			},
+		},
+	},
+};
